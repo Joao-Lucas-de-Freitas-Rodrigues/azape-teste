f79a604f567866b7fefb4c5dd1ac247a906ad4a9
Lucas Freitas <joaolucasfreitas360@gmail.com>
2025-03-15 03:32:14 -0300

    feat: Utilizando Forms e fazendo a chamada da função de login

--- Frontend/azape-frontend/src/app/pages/login/login.component.ts
+++ Frontend/azape-frontend/src/app/pages/login/login.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
 
+  constructor(private authService: AuthService, private router: Router) { }
+
+  onLogin(): void {
+    this.authService.login(this.email, this.password).subscribe({
+      next: (response) => {
+        
+        console.log(response)
+        if (response.token) {
+          localStorage.setItem('token', response.token);
+          localStorage.setItem('username', response.profile.name);
+          this.router.navigate(['/dashboard']);
+        } else {
+          this.errorMessage = 'Falha no login. Verifique suas credenciais.';
+        }
+      },
+      error: (err) => {
+        console.error('Erro de login:', err);
+        this.errorMessage = 'Falha no login. Verifique suas credenciais.';
+      }
+    });
+  }
 }
