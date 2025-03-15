73dc34a3abc80b7c9c84d749801b3c014d26332a
Lucas Freitas <joaolucasfreitas360@gmail.com>
2025-03-15 02:51:33 -0300

    feat: Ajustando app.routes para validar rotas

--- Frontend/azape-frontend/src/app/app.routes.ts
+++ Frontend/azape-frontend/src/app/app.routes.ts
@@ -1,7 +1,18 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AuthGuard } from './services/guard/auth.guard';
+import { LoginGuard } from './services/guard/login.guard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent }
-];
+    { path: 'login', 
+      component: LoginComponent, 
+      canActivate: [LoginGuard] },
+    {
+      path: 'dashboard',
+      component: DashboardComponent,
+      canActivate: [AuthGuard]
+    },
+    { path: '**', redirectTo: 'login' }
+  ];
+  
